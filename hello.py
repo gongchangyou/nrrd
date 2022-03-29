f7c7b4cde2e5b21dfbf771ffc8b26a8a9f128de7
gongchangyou <gongchangyou@58.com>
2022-03-29 21:21:55 +0800

    test

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ print(help(simple))
 print("Hello, World!");
 
 '''
-nrrdFilepath="/tmp/tracker-extract-files.125";
-downsampledFilePath="/tmp/tracker-extract-files.125.down";
-subprocess.call(['unu', 'resample', '-i', nrrdFilepath, '-o', downsampledFilePath, '-s', 'x0.5', 'x0.5', 'x0.5'])
+nrrdFilepath="/tmp/test";
+resampledFilePath="/tmp/test.down";
+subprocess.call(['unu', 'resample', '-i', nrrdFilepath, '-o', resampledFilePath, '-s', 'x0.5', 'x0.5', 'x0.5'])
 '''
